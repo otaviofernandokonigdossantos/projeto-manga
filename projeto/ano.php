517f9617ef539073d41165dfaae54cf683308e72
otaviofernandokonigdossantos <150362846+otaviofernandokonigdossantos@users.noreply.github.com>
2024-05-14 11:34:42 -0300

    Add files via upload

---
+++ projeto/ano.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="paginaprincipal.php"><button>voltar</button></a><br><br>  
+    <style>
+        body{
+            background-image: url(../projeto/img/winner.webp);
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+        button{
+            background-color: rgb(255, 115, 0);
+        }
+    </style>
+</body>
+</html>
---
+++ projeto/paginaprincipal.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>projeto A</title>
+    <link rel="stylesheet" href="paginaprincipal.css">
+   
+</head>
+<body>
+    <div class="a">
+       <div class="Nometext-center">
+        <h1>PROJETO A</h1>
+
+    </div>
+    <header>
+        
+        <div>
+            <a href="index.php"><button>voltar</button></a><br><br>  
+
+        </div>
+        <nav>
+            <ul>
+              <li><a href="mangás.php">Mangás</a></li>
+              <li><a href="#">Genêros</a></li>
+              <li><a href="index.php">Inicio</a></li>
+              <li><a href="em altas.php">Em Altas</a></li>
+              <li><a href="ano.php">Ano</a></li>
+            </ul>
+          </nav>
+          
+        <div>
+            <img src="img/th.jpeg" alt="Your image description" class="rounded-image">
+                    <h2>Abner2607</h2>
+                   
+        </div>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                
+            </div>
+        
+      
+        </div>
+    </div>
+    
+    </header>
+                        
+</body>
+</html>
