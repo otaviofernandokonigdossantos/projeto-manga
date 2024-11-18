90dbd90ad8df2f3f522265006eaa48159df4d77e
otaviofernandokonigdossantos <150362846+otaviofernandokonigdossantos@users.noreply.github.com>
2024-11-18 10:57:34 -0300

    Add files via upload

---
+++ paginas/colab.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="colab.css">
+</head>
+<body>
+<h1>PROJETO A</h1>
+    <a href="paginaprincipal.php"><button>voltar</button></a><br><br>  
+    <div class="manga-reader">
+        <img src="capitulos/00.jpg">
+        <img src="capitulos/01.jpg">
+        <img src="capitulos/02.jpg">
+        <img src="capitulos/03.jpg">
+        <img src="capitulos/04.jpg">
+        <img src="capitulos/05.jpg">
+        <img src="capitulos/06.jpg">
+        <img src="capitulos/07.jpg">
+        <img src="capitulos/08.jpg">
+        <img src="capitulos/09.jpg">
+        <img src="capitulos/010.jpg">
+        <img src="capitulos/011.jpg">
+        <img src="capitulos/012.jpg">
+        <img src="capitulos/013.jpg">
+        <img src="capitulos/014.jpg">
+        <img src="capitulos/015.jpg">
+        <img src="capitulos/016.jpg">
+        <img src="capitulos/017.jpg">
+        <img src="capitulos/018.jpg">
+        <img src="capitulos/019.jpg">
+        <img src="capitulos/020.jpg">
+        <img src="capitulos/021.jpg">
+        <img src="capitulos/022.jpg">
+        <img src="capitulos/023.jpg">
+        <img src="capitulos/024.jpg">
+        <img src="capitulos/025.jpg">
+        <img src="capitulos/026.jpg">
+        <img src="capitulos/027.jpg">
+        <img src="capitulos/028.jpg">
+        <img src="capitulos/029.jpg">
+        <img src="capitulos/030.jpg">
+        <img src="capitulos/031.jpg">
+        <img src="capitulos/032.jpg">
+        <img src="capitulos/033.jpg">
+        <img src="capitulos/034.jpg">
+        <img src="capitulos/035.jpg">
+        <img src="capitulos/036.jpg">
+        <img src="capitulos/037.jpg">
+        <img src="capitulos/038.jpg">
+        <img src="capitulos/039.jpg">
+        <img src="capitulos/040.jpg">
+        <img src="capitulos/041.jpg">
+        <img src="capitulos/042.jpg">
+        <img src="capitulos/043.jpg">
+        <img src="capitulos/044.jpg">
+        <img src="capitulos/045.jpg">
+        <img src="capitulos/046.jpg">
+        <img src="capitulos/047.jpg">
+        <img src="capitulos/048.jpg">
+</div>
+    <a href="colab2.php"><button>proximo</button></a>
+</body>
+</html>
---
+++ paginas/colab2.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="colab2.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="manga-reader">
+    <a href="colab.php"><button>Voltar</button></a><br><br>
+        <img src="capitulos/1.jpg">
+        <img src="capitulos/2.jpg">
+        <img src="capitulos/3.jpg">
+        <img src="capitulos/4.jpg">
+        <img src="capitulos/5.jpg">
+        <img src="capitulos/6.jpg">
+        <img src="capitulos/7.jpg">
+        <img src="capitulos/8.jpg">
+        <img src="capitulos/9.jpg">
+        <img src="capitulos/10.jpg">
+        <img src="capitulos/11.jpg">
+        <img src="capitulos/12.jpg">
+        <img src="capitulos/13.jpg">
+        <img src="capitulos/14.jpg">
+        <img src="capitulos/15.jpg">
+        <img src="capitulos/16.jpg">
+        <img src="capitulos/17.jpg">
+        <img src="capitulos/18.jpg">
+        <img src="capitulos/19.jpg">
+        <img src="capitulos/20.jpg">
+        <img src="capitulos/21.jpg">
+        <img src="capitulos/22.jpg">
+        <img src="capitulos/23.jpg">
+        <img src="capitulos/24.jpg">
+        <img src="capitulos/25.jpg">
+        <img src="capitulos/26.jpg">
+        <img src="capitulos/27.jpg">
+        <img src="capitulos/28.jpg">
+        <img src="capitulos/29.jpg">
+        <img src="capitulos/30.jpg">
+        <img src="capitulos/31.jpg">
+        <img src="capitulos/32.jpg">
+        <img src="capitulos/33.jpg">
+        <img src="capitulos/34.jpg">
+        <img src="capitulos/35.jpg">
+        <img src="capitulos/36.jpg">
+        <img src="capitulos/37.jpg">
+        <img src="capitulos/38.jpg">
+        
+        
+</div>
+</body>
+</html>
